5b3dc33bf51ed6ea013e0ad119a34e68e0d7a226
Overlord7667 <86426720+Overlord7667@users.noreply.github.com>
2023-06-05 23:54:05 +0300

    Add files via upload

---
+++ AdapterDZ/AdapterDZ/TextGraphics.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterDZ
+{
+    class TextGraphics
+    {
+        Graphics _graphics;
+        TextView _textView;//Подключен IO
+        public TextGraphics(Graphics graphics, TextView textView)
+        {
+            _textView = textView;
+            _graphics = graphics;
+        }
+    }
+}
---
+++ AdapterDZ/AdapterDZ/TextView.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterDZ
+{
+    class TextView
+    {
+        public int[,] Area { get; set; }
+        public int N { get; set; }
+        public int Count { get; set; }
+        public TextView(int n=4)
+        {
+            N = n;
+            Area = new int[N, N];
+        }
+        public void LoadFromFile(string FileName)
+        {
+            TextMemento textMemento = new TextAdapterFromFile(FileName);
+            N = textMemento.N;
+            Count = textMemento.Count;
+            Area = textMemento.Area;
+        }
+    }
+}
