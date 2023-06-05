5a0021109243ae82b05da92d830d24a47f3d4e44
Overlord7667 <86426720+Overlord7667@users.noreply.github.com>
2023-06-06 00:03:46 +0300

    Add files via upload

---
+++ MVVM/MVVM/ApplicationViewModel.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
+
+namespace MVVM
+{
+    internal class ApplicationViewModel : INotifyPropertyChanged
+    {
+        Phone selectedPhone;
+
+        public ObservableCollection<Phone> Phones { get; set; }
+        private RelayCommand? addCommand;
+        public RelayCommand AddCommand
+        {
+            get
+            {
+                return addCommand ??
+                    (addCommand = new RelayCommand(obj =>
+                    {
+                        Phone phone = new Phone("", "", 0);
+                        Phones.Insert(0, phone);
+                        SelectedPhone = phone;
+                    }));
+            }
+        }
+        private RelayCommand? removeCommand;
+        public RelayCommand RemoveCommand
+        {
+            get
+            {
+                return removeCommand ??
+                    (removeCommand = new RelayCommand(obj =>
+                    {
+                        Phone phone = obj as Phone;
+                        if (phone != null)
+                        {
+                            Phones.Remove(phone);
+                        }
+                    },
+                (obj) => Phones.Count > 0));
+            }
+        }
+        private RelayCommand? doubleCommand;
+        public RelayCommand DoubleCommand
+        {
+            get
+            {
+                return doubleCommand ??
+                    (doubleCommand = new RelayCommand(obj =>
+                    {
+                        Phone? phone = obj as Phone;
+                        if (phone != null)
+                        {
+                            Phone phoneCopy = new Phone (phone.Title, phone.Company, phone.Price);
+                            Phones.Insert(0, phoneCopy);
+                        }
+                    }));
+
+            }
+        }
+
+        public Phone? SelectedPhone
+        {
+            get { return selectedPhone; }
+            set
+            {
+                selectedPhone = value;
+                OnPropertyChanged( "SelectedPhone");
+            }
+        }
+
+        public ApplicationViewModel()
+        {
+            Phones = new ObservableCollection<Phone>
+            {
+                new Phone( "IPhone 14", "Apple", 125000),
+                new Phone("Galaxy s22 Ultra", "Samsung", 95000),
+                new Phone("12 Lite", "Xiaomi", 35000)
+            };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+    
+}
---
+++ MVVM/MVVM/JsonFileService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM
+{
+    class JsonFileService:IFileService
+    {
+        public List<Phone>Open(string filename)
+        {
+            List<Phone> phones = new List<Phone>();
+            DataContractJsonSerializer jsonFaramatter=new DataContractJsonSerializer(typeof(Phone));
+            using (FileStream fs = new FileStream (filename, FileMode.OpenOrCreate))
+            {
+                phones=jsonFaramatter.ReadObject(fs) as List<Phone>;
+            }
+            return phones;
+        }
+        public void Save(string filename, List<Phone> phoneList)
+        {
+            DataContractJsonSerializer jsonFaramatter = new DataContractJsonSerializer(typeof(Phone));
+            using (FileStream fs = new FileStream(filename, FileMode.Create))
+            {
+                jsonFaramatter.WriteObject(fs, phoneList);
+            }
+            
+        }
+    }
+}
