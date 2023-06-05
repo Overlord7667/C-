7b224b8749c3424bf2739ed1bff3534a2e27bd04
Overlord7667 <86426720+Overlord7667@users.noreply.github.com>
2023-06-06 00:00:29 +0300

    Add files via upload

---
+++ TelephoneBook1/TelephoneBook1/ContactViewModel.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace TelephoneBook1
+{
+    class ContactViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void Notify(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        List<Contact> contacts;
+        Contact contact;
+        PersonModel personModel;
+        public ContactViewModel()
+        {
+            personModel = new PersonModel();
+            contact = new Contact();
+            if(personModel.GetAllContacts() != null)
+            {
+                contacts = new List<Contact>(personModel.GetAllContacts());
+            }
+            else
+            {
+                contacts = new List<Contact>();
+            }
+        }
+
+        public List<Contact> Contacts
+        {
+            get { return contacts; }
+            set
+            {
+                contacts = value;
+                Notify("Contacts");
+            }
+        }
+        public Contact SelectedContact
+        {
+            get { return contact; }
+            set
+            {
+                contact = value;
+                Notify("SelectedContact");
+            }
+        }
+
+        public ICommand AddButton
+        {
+            get
+            {
+                return new ButtonComand(new Action(() =>
+                {
+                    AddContact addContact = new AddContact();
+                    addContact.ShowDialog();
+                    Contacts = personModel.GetAllContacts();
+                }));
+            }
+        }
+
+        public ICommand DeleteButton
+        {
+            get
+            {
+                return new ButtonComand(new Action(() =>
+                {
+                    if(contact != null)
+                    {
+                        MessageBoxResult result = MessageBox.Show("Вы действительно хотите удалить контакт?",
+                            "Удаление контакта " + contact.FirstName, MessageBoxButton.YesNo);
+                        if (result == MessageBoxResult.Yes)
+                        {
+                            personModel.DeleteContact(contact);
+                        }
+                        Contacts = personModel.GetAllContacts();
+                    }
+                }));
+            }
+        }
+
+        public ICommand UpdateButton
+        {
+            get
+            {
+                return new ButtonComand(new Action(() =>
+                {
+                    if(contact != null)
+                    {
+                        UpdateViewModel.Id = contact.Id;
+                        UpdateContact updateContact = new UpdateContact();
+                        updateContact.ShowDialog();
+                        Contacts = personModel.GetAllContacts();
+                    }
+                }));
+            }
+        }
+
+    }
+}
---
+++ TelephoneBook1/TelephoneBook1/PersonModel.cs
@@ -0,0 +1,115 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Windows;
+
+namespace TelephoneBook1
+{
+    class PersonModel : IPersonModel
+    {
+        MySqlConnection _connection;
+        List<Contact> _contacts;
+        
+
+        public PersonModel()
+        {
+            _connection = new MySqlConnection(
+                "Server=localhost;database=test_data;Uid=mysql");
+            _contacts = new List<Contact>();
+        }
+        void ExecuteCommand(string query)
+        {
+            MySqlCommand command = new MySqlCommand(query, _connection);
+            _connection.Open();
+            command.ExecuteNonQuery();
+            _connection.Close();
+        }
+        public void AddContact(Contact contact)
+        {
+            string query = $"INSERT INTO contacts (first_name, last_name, number_phone, images, note, birthd_date) " +
+                $"VALUES ('{contact.FirstName}', '{contact.LastName}', '{contact.Phone}', '{contact.ImageUri}', '{contact.Note}', '{contact.BDay:yyyy-MM-dd}')";
+            ExecuteCommand(query);
+        }
+        public void DeleteContact(Contact contact)
+        {
+            string query = $"DELETE FROM contacts WHERE id=" + contact.Id.ToString();
+            ExecuteCommand(query);
+        }
+        public Contact GetContact(int id)
+        {
+            string query = $"SELECT * FROM contacts WHERE id= {id}";
+            Contact contact = new Contact();
+            MySqlDataAdapter adapter = new MySqlDataAdapter(query, _connection);
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+            if(table.Rows.Count > 0)
+            {
+                contact.Id = (int)table.Rows[0].ItemArray[0];
+                contact.FirstName = table.Rows[0].ItemArray[1].ToString();
+                contact.LastName = table.Rows[0].ItemArray[2].ToString();
+                contact.Phone = table.Rows[0].ItemArray[3].ToString();
+                contact.ImageUri = table.Rows[0].ItemArray[4].ToString();
+                contact.Note = table.Rows[0].ItemArray[5].ToString();
+                contact.BDay = DateTime.Parse(table.Rows[0].ItemArray[6].ToString());
+            }
+            return contact;
+        }
+        public void UpdateContact(Contact first, Contact result)
+        {
+            string query = $"UPDATE contacts SET first_name='{result.FirstName}', last_name='{result.LastName}', number_phone='{result.Phone}', images='{result.ImageUri}', " +
+                $"note='{result.Note}', birthd_date='{result.BDay:yyyy-MM-dd}' WHERE id={first.Id}";
+            ExecuteCommand(query);
+        }
+        public Person GetLogin(string login, string password)
+        {
+            Person _person = new Person();
+            string connectString = "Server=Localhost;Database=test_data;Uid=mysql";
+            MySqlConnection _connection = new MySqlConnection(connectString);
+            _connection.Open();
+            string query = $"SELECT* FROM users WHERE login = '{login}' AND password = '{password}'";
+            MySqlDataAdapter adapter = new MySqlDataAdapter(query, _connection);
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+            if (table.Rows.Count > 0)
+            {
+                _person.Id = (int)table.Rows[0].ItemArray[0];
+                _person.Header = table.Rows[0].ItemArray[1].ToString();
+                _person.Text = table.Rows[0].ItemArray[2].ToString();
+                _person.DeadLine = DateTime.Parse(table.Rows[0].ItemArray[3].ToString());
+            }
+            _connection.Close();
+            return _person;
+        }
+
+        public List<Contact> GetAllContacts()
+        {
+            _contacts = new List<Contact>();
+            string connectString = "Server=Localhost;Database=test_data;Uid=mysql";
+            MySqlConnection _connection = new MySqlConnection(connectString);
+            _connection.Open();
+            string query = $"SELECT* FROM contacts";
+            MySqlDataAdapter adapter = new MySqlDataAdapter(query, _connection);
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+            for (int i = 0; i < table.Rows.Count; i++)
+            {
+                _contacts.Add(new Contact()
+                {
+                    Id = (int)table.Rows[i].ItemArray[0],
+                    FirstName = table.Rows[i].ItemArray[1].ToString(),
+                    LastName = table.Rows[i].ItemArray[2].ToString(),
+                    Phone = table.Rows[i].ItemArray[3].ToString(),
+                    ImageUri = table.Rows[i].ItemArray[4].ToString(),
+                    Note = table.Rows[i].ItemArray[5].ToString(),
+                    BDay = DateTime.Parse(table.Rows[i].ItemArray[6].ToString())
+                });
+            }
+            _connection.Close();
+            return _contacts;
+        }
+    }
+}
